7fdf35ae87f1b0ce3bd888203f8fac6b2df94fc7
johnbhd <villegasjohnb18@gmail.com>
2024-12-20 13:53:45 +0800

    12-20-24

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,14 @@
-// Your main.js content 
-    
+// pages
+let singlep = document.getElementById('playerone');
+let multip = document.getElementById('playertwo');
+let help = document.getElementById('help');
+
+singlep.onclick = () => {
+   window.location = "./pages/singleplayer/home.html";
+}
+multip.onclick = () => {
+   window.location = "./pages/multiplayer/home.html";
+}
+help.onclick = () => {
+   window.location = "./pages/help/help.html";
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
-    './src/**/*.{html,js}',
-    './public/index.html',
+    './src/**/*.{html,js}',             // Includes HTML and JS files in src folder
+    './public/index.html',              // Directly includes the public index.html
+    './public/**/*.{html}',             // Includes all HTML files in the public folder (and subfolders)
   ],
   theme: {
     extend: {},
